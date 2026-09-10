c3cf1e71d420d3728895bdc7d62689c554423755
robinhermes694 <robinhermes694@users.noreply.github.com>
2026-09-09 23:12:10 -0700

    Fix zsh syntax highlighting: set ZSH_HIGHLIGHT_STYLES and highlighters before plugin source
    The zsh-syntax-highlighting plugin from home-manager sources the plugin after
    initContent, but ZSH_HIGHLIGHT_HIGHLIGHTERS was set after the source (by
    home-manager) and ZSH_HIGHLIGHT_STYLES for commands/aliases/builtins was
    never configured. This made syntax highlighting appear 'off' for common
    commands like ls, cat, etc.
    
    Fix: declare ZSH_HIGHLIGHT_STYLES as associative array and set the missing
    styles (command, alias, builtin, function, etc.) in initContent so they
    are available when the plugin is sourced.

--- home.nix
+++ home.nix
@@ -39,6 +39,19 @@ in
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
     initContent = ''
+      # Set ZSH_HIGHLIGHT_STYLES before zsh-syntax-highlighting is sourced
+      # (home-manager sources the plugin after initContent, so these take effect)
+      typeset -gA ZSH_HIGHLIGHT_STYLES
+      ZSH_HIGHLIGHT_HIGHLIGHTERS=(main)
+      ZSH_HIGHLIGHT_STYLES[command]='fg=cyan,underline'
+      ZSH_HIGHLIGHT_STYLES[alias]='fg=cyan,underline'
+      ZSH_HIGHLIGHT_STYLES[builtin]='fg=cyan,underline'
+      ZSH_HIGHLIGHT_STYLES[function]='fg=cyan,underline'
+      ZSH_HIGHLIGHT_STYLES[command_separator]='fg=yellow'
+      ZSH_HIGHLIGHT_STYLES[su]=none
+      ZSH_HIGHLIGHT_STYLES[parent]='fg=magenta'
+      ZSH_HIGHLIGHT_STYLES[path]='fg=cyan,underline'
+
       bindkey '^f' autosuggest-accept
       bindkey -v
       setopt inc_append_history
